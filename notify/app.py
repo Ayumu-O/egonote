170a44655d58a3eaddc94e5f8deb9b315363ebff
ayumu <ayumu4bus@gmail.com>
2023-05-07 23:46:26 +0900

    feat notify func

---
+++ notify/app.py
@@ -0,0 +1,19 @@
+import os
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.models import (
+    TextSendMessage,
+)
+
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+
+
+def lambda_handler(event, context):
+    line_bot_api.broadcast(
+        messages=TextSendMessage(
+            text='今日の「できたこと」を 1 ~ 3 つ教えてください！'
+        )
+    )
